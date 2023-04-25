cc0718804a8a531413d3944622ae6569b46a7669
zygarte <zygarte@gmail.com>
2023-04-25 17:20:21 -0500

    Prototipo base

--- Encriptador de texto/script.js
+++ Encriptador de texto/script.js
@@ -1,70 +1,90 @@
-// Obtener elementos del DOM
-const input = document.getElementById("input");
-const output = document.getElementById("output");
-const btnEncrypt = document.getElementById("btn-encrypt");
-const btnDecrypt = document.getElementById("btn-decrypt");
-const btnCopy = document.getElementById("btn-copy");
-
-// Mapa de encriptación
-const encryptionMap = {
-  e: "enter",
-  i: "imes",
-  a: "ai",
-  o: "ober",
-  u: "ufat",
-};
-
-// Función para encriptar texto
-function encryptText(text) {
-  let encryptedText = "";
-  for (let i = 0; i < text.length; i++) {
-    const char = text.charAt(i);
-    encryptedText += encryptionMap[char] || char;
+function convertText() {
+    var inputText = document.getElementById("input-text").value;
+    var outputText = "";
+    var mode = document.querySelector('input[name="mode"]:checked').value;
+  
+    if (mode === "encrypt") {
+      outputText = encryptText(inputText);
+    } else if (mode === "decrypt") {
+      outputText = decryptText(inputText);
+    }
+  
+    document.getElementById("output-text").value = outputText;
   }
-  return encryptedText;
-}
-
-// Función para desencriptar texto
-function decryptText(text) {
-  let decryptedText = "";
-  let i = 0;
-  while (i < text.length) {
-    const char = text.charAt(i);
-    if (char in encryptionMap) {
-      const possibleDecryptedChar = text.substr(i, encryptionMap[char].length);
-      if (encryptionMap[char] === possibleDecryptedChar) {
-        decryptedText += char;
-        i += encryptionMap[char].length;
-        continue;
+  
+  function encryptText(text) {
+    var encryptedText = "";
+    var valueToLetter = {
+      "e": "enter",
+      "i": "imes",
+      "a": "ai",
+      "o": "ober",
+      "u": "ufat"
+    };
+    for (var i = 0; i < text.length; i++) {
+      var letter = text.charAt(i);
+      if (letter.toLowerCase() !== letter) {
+        alert("Solo se permiten letras minúsculas.");
+        return "";
+      }
+      if (/^[a-zA-Z ]*$/.test(letter) === false) {
+        alert("No se permiten caracteres especiales.");
+        return "";
+      }
+      if (letter in valueToLetter) {
+        encryptedText += valueToLetter[letter];
+      } else {
+        encryptedText += letter;
       }
     }
-    decryptedText += char;
-    i++;
+    return encryptedText;
   }
-  return decryptedText;
-}
-
-// Función para copiar el texto al portapapeles
-function copyText(text) {
-  const textarea = document.createElement("textarea");
-  textarea.value = text;
-  document.body.appendChild(textarea);
-  textarea.select();
-  document.execCommand("copy");
-  document.body.removeChild(textarea);
-}
-
-// Evento de click en botón "Encriptar"
-btnEncrypt.addEventListener("click", () => {
-  output.value = encryptText(input.value);
-});
-
-// Evento de click en botón "Desencriptar"
-btnDecrypt.addEventListener("click", () => {
-  output.value = decryptText(input.value);
-});
-
-// Evento de click en botón "Copiar"
-btnCopy.addEventListener("click", () => {
-  copyText(output.value);
-});
+  
+  function decryptText(text) {
+    var decryptedText = "";
+    var letterToValue = {
+      "enter": "e",
+      "imes": "i",
+      "ai": "a",
+      "ober": "o",
+      "ufat": "u"
+    };
+    var i = 0;
+    while (i < text.length) {
+      var found = false;
+      for (var len = 5; len >= 1; len--) {
+        var substr = text.substr(i, len).toLowerCase();
+        if (substr in letterToValue) {
+          decryptedText += letterToValue[substr];
+          i += len;
+          found = true;
+          break;
+        }
+      }
+      if (!found) {
+        var letter = text.charAt(i);
+        if (letter.toLowerCase() !== letter) {
+          alert("Solo se permiten letras minúsculas.");
+          return "";
+        }
+        if (/^[a-zA-Z ]*$/.test(letter) === false) {
+          alert("No se permiten caracteres especiales.");
+          return "";
+        }
+        decryptedText += letter;
+        i++;
+      }
+    }
+    return decryptedText;
+  }
+  
+  
+  
+  
+  
+  function copyText() {
+    var outputText = document.getElementById("output-text");
+    outputText.select();
+    document.execCommand("copy");
+  }
+  
